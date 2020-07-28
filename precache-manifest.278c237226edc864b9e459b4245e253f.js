84aea6933dfe3c757df91e19af857a6b1529353c
Adam Horscraft <Adam@Adams-MacBook-Pro-2.local>
2020-07-28 16:42:33 +0100

    Updates

---
+++ precache-manifest.278c237226edc864b9e459b4245e253f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4920997671ccf9990f5d40b56d51b1c6",
+    "url": "/addressBook/index.html"
+  },
+  {
+    "revision": "77c7744e314932985729",
+    "url": "/addressBook/static/css/main.c4dffe56.chunk.css"
+  },
+  {
+    "revision": "8361d700ea3eae3123f2",
+    "url": "/addressBook/static/js/2.ce13f874.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/addressBook/static/js/2.ce13f874.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77c7744e314932985729",
+    "url": "/addressBook/static/js/main.29b09099.chunk.js"
+  },
+  {
+    "revision": "4e4a41b274b5dd229ce7",
+    "url": "/addressBook/static/js/runtime-main.8daeb4fb.js"
+  }
+]);
