0673b2b287c8a8eaf865a98bf79c62a61bd4186c
Adam Horscraft <Adam@Adams-MacBook-Pro-2.local>
2020-07-28 16:31:05 +0100

    this is my commit comment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BookItems from './BookItems/BookItems'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    this.setUpdate = this.setUpdate.bind(this);
+  }
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if (newItem.text !== "") {
+      const items = [...this.state.items, newItem];
+      this.setState({
+        items: items,
+        currentItem: {
+          text: '',
+          key: ''
+        }
+      })
+    }
+  }
+  handleInput(e) {
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  deleteItem(key) {
+    const filteredItems = this.state.items.filter(item =>
+      item.key !== key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+  setUpdate(text, key) {
+    console.log("items:" + this.state.items);
+    const items = this.state.items;
+    items.map(item => {
+      if (item.key === key) {
+        console.log(item.key + "    " + key)
+        item.text = text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder="Enter details" value={this.state.currentItem.text} onChange={this.handleInput}></input>
+            <button type="submit" id='btn'>Add</button>
+          </form>
+          <p>{this.state.items.text}</p>
+
+          <BookItems items={this.state.items} deleteItem={this.deleteItem} setUpdate={this.setUpdate} />
+
+        </header>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/BookItems/BookItems.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import BookItems from './BookItems.css';
+
+function AddressBook(props) {
+    const items = props.items;
+    const listItems = items.map(item => {
+        return <div className="list" key={item.key}>
+            <p>
+                <input type="text" id={item.key} value={item.text} onChange={(e) => {
+                    props.setUpdate(e.target.value, item.key)
+                }} />
+                <span>
+
+                    <button className="faicons" onClick={() => {
+                        props.deleteItem(item.key)
+                    }} />
+                </span>
+            </p>
+
+        </div>
+    })
+    return <div>
+
+        {listItems}
+
+
+    </div>;
+}
+
+export default AddressBook;
